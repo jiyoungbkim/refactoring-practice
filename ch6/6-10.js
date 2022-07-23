b141a7aaacd5073ada1407958fad710b364dbf9e
jiyoungbkim <jiyoungbkim@gmail.com>
2022-07-23 21:32:16 +0900

    6.10 Combine Functions into Transform

--- ch6/6-10.js
+++ ch6/6-10.js
@@ -1,10 +1,30 @@
-const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
+import _ from "lodash";
+
+const reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
   return reading;
 }
 
+export function enrichReading(original) {
+  // const result = { ...original }; // Object.assign: 얕은 복사
+  const result = _.cloneDeep(original); // 깊은 복사 새로운 객체로 복사해줌
+  result.baseCharge = caculateBaseCharge(result);
+  result.taxableCharge = Math.max(
+    0,
+    result.baseCharge - taxThreshold(result.year)
+  );
+  return result;
+}
+function caculateBaseCharge(reading) {
+  return baseRate(reading.month, reading.year) * reading.quantity;
+}
+
 export function baseRate(month, year) {
   if (year === 2017 && month === 5) return 0.1;
   return 0.2;
+}
+
+function taxThreshold() {
+  return 0.1;
 }
