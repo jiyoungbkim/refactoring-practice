1f7585d18908b09e41fb1006b788ea01760f27aa
jiyoungbkim <jiyoungbkim@gmail.com>
2022-07-23 20:30:33 +0900

    6.6 Encapsulate Variable

--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,4 +1,19 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+class Person {
+  #lastName;
+  #firstName;
+  constructor(data) {
+    this.#firstName = data.firstName;
+    this.#lastName = data.lastName;
+  }
+  get lastName() {
+    return this.#lastName;
+  }
+  get firstName() {
+    return this.#firstName;
+  }
+}
+
+let defaultOwner = new Person({ firstName: "마틴", lastName: "파울러" });
 
 export function getDefaultOwner() {
   return defaultOwner;
