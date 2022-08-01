34b8d063495984fe67b7c0f9f4833ada35c2cc0f
jiyoungbkim <jiyoungbkim@gmail.com>
2022-08-01 21:15:02 +0900

    7.1 Encapsulate Record

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,35 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #data;
+  #name;
+  #country;
+  constructor(data) {
+    this.#data = data;
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+  get name() {
+    return this.#name;
+  }
+  set name(value) {
+    this.#name = value;
+  }
+  get country() {
+    return this.#country;
+  }
+  set country(value) {
+    this.#country = value;
+  }
+  get rawData() {
+    // return { ...this.#data }; // 얕은 복사 혹은 cloneDeep이용 // get만 있는 경우
+    return { name: this.name, country: this.country }; // set도 있는 경우
+  }
+}
 
-organization.name = 'Dream Coding';
+const organization = new Organization({
+  name: "Acme Gooseberries",
+  country: "GB",
+});
+
+organization.name = "Dream Coding";
 console.log(organization.name);
 console.log(organization.country);
