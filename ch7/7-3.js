1eec79d17aac2e418d2cfbf84f90e27f26aab85a
jiyoungbkim <jiyoungbkim@gmail.com>
2022-08-01 21:16:10 +0900

    7.3 Replace Primitive with Object

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -2,14 +2,44 @@ export class Order {
   constructor(data) {
     this.priority = data.priority;
   }
+
+  isHighPriority() {
+    return "high" === this.priority || "rush" === this.priority;
+    // return this.priority.higherThan("normal");
+  }
 }
 
-const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
-];
+// class Priority {
+//   #value;
+//   constructor(value) {
+//     if (Priority.legalValues().includes(value)) {
+//       this.#value = value;
+//     } else {
+//       throw new Error(`${value} is invalid for Priority`);
+//     }
+//   }
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
+//   get index() {
+//     return Priority.legalValues().indexOf(this.#value);
+//   }
+
+//   equals(other) {
+//     return this.index === other.index;
+//   }
+
+//   higherThan(other) {
+//     return this.index > other.index;
+//   }
+
+//   static legalValues() {
+//     return ["low", "normal", "high", "rush"];
+//   }
+// }
+
+const orders = [
+  new Order({ priority: "normal" }),
+  new Order({ priority: "high" }),
+  new Order({ priority: "rush" }),
+];
+const highPriorityCount = orders.filter((o) => o.isHighPriority()).length;
+console.log(highPriorityCount);
